2175ebe688576e3ce42c21eff5a2cbe4f2e72220
gepoweibu <1227280057@qq.com>
2019-01-01 01:10:34 +0800

    test delete File

--- selfstudy/src/study201812_03/FileIO.java
+++ selfstudy/src/study201812_03/FileIO.java
@@ -12,8 +12,9 @@ public class FileIO {
 
 	public static void main(String[] args) throws IOException, URISyntaxException {
 		testFile();
+		//delTestOutputFile();
 	}
-	//File
+	//File,读取文件内容输出到另一个文件中
 	public static void testFile() throws IOException, URISyntaxException{
 		URL url = FileIO.class.getResource("testIOFile");//获取文件url的方式---读取的是编译后的文件路径，在Bin目录下
 		System.out.println("文件URL----"+url);
@@ -27,7 +28,7 @@ public class FileIO {
 		//为了把文件放到Src下，把文件路劲中的bin替换为src
 		String opath = f.getParent().replaceAll("bin", "src");
 		System.out.println("output path : "+opath);
-		FileOutputStream fo = new FileOutputStream(f.getParent().replaceAll("bin", "src")+"/testIOfile_O");
+		FileOutputStream fo = new FileOutputStream(f.getParent().replaceAll("/bin/", "/src/")+"/testIOfile_O");
 		byte[] buffer=new byte[512];   //一次取出的字节数大小,缓冲区大小  
 	    int numberRead=0;  
 	    while ((numberRead=fi.read(buffer))!=-1) {  //numberRead的目的在于防止最后一次读取的字节小于buffer长度，  
@@ -37,5 +38,23 @@ public class FileIO {
 	    fo.close();
 	    System.out.println("------in this path check the testIOfile_O file----");
 	}
+    //删除testOfile_O测试输出文件
+	public static void delTestOutputFile(){
+		URL url = FileIO.class.getResource("testIOFile");
+		String ipath=url.toString().substring(url.toString().indexOf("/")+1);
+		System.out.println("读取文件的路劲: "+ipath);
+		String opath = ipath.substring(0, ipath.lastIndexOf("/")).replaceAll("/bin/", "/src/");
+		System.out.println("写入文件的路劲: "+opath);
+		System.out.println("---删除文件 : "+opath+"/testIOFile_O");
+		//删除文件
+		delFile(opath,"testIOFile_O");
+		System.out.println("----testIOFile_O--文件已删除--到文件目录下去查验-----");
+	}
+	//删除文件
+	public static void delFile(String path,String filename){
+        File file=new File(path+"/"+filename);
+        if(file.exists()&&file.isFile())
+            file.delete();
+    }
 
 }
